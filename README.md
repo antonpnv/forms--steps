c63397af046ff49ee510a630b415fbc1b1d88ffb
AntonPnv <anton.panov992@gmail.com>
2024-06-18 15:51:52 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# forms
+
+## Учёт тренировок
+
+[![Build status](https://ci.appveyor.com/api/projects/status/j98xe1a9x2hyvdth?svg=true)](https://ci.appveyor.com/project/antonpnv/forms-steps)
+
+![CI](https://github.com/antonpnv/forms--steps/actions/workflows/web.yml/badge.svg)
---
+++ src/components/StepsRender/StepsRender.tsx
@@ -0,0 +1,31 @@
+import { IStepsRender } from "../../models";
+
+export const StepsRender: React.FC<IStepsRender> = ({ steps, onDelete, onEdit }) => {
+  const formatDate = (date: string): string => {
+    const parts = date.split('-');
+    return `${parts[2]}.${parts[1]}.${parts[0]}`;
+  };
+
+  return (
+    <>
+      <div className="table">
+        <div className="date__title">Дата (ДД.ММ.ГГ)</div>
+        <div className="distance__title">Пройдено км</div>
+        <div className="actions__title">Действия</div>
+      </div>
+
+      <div className={`result ${steps.length > 0 ? 'result--bordered' : ''}`}>
+        {steps.map((step) => (
+          <div key={step.date}>
+            <div className="date__result">{formatDate(step.date)}</div>
+            <div className="distance__result">{step.distance}</div>
+            <div className="btns">
+              <button className="edit__btn" onClick={() => onEdit(step.date)}>✎</button>
+              <button className="delete__btn" onClick={() => onDelete(step.date)}>✘</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
